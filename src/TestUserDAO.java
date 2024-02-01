8a86d7c139b81fe00c63bf90a50e528c21404454
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-01 20:38:57 +0900

    5:プログラム追加

--- src/TestUserDAO.java
+++ src/TestUserDAO.java
@@ -37,5 +37,31 @@ public class TestUserDAO {
 		}
 
 	}
+	
+	public void serectAll() {
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+		
+		String sql = "select * from test_table";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println(rs.getString("user_name"));
+				System.out.println(rs.getString("password"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			con.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
