463b7be85406c93756c3e982c9065b5d3f5820de
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-01 22:23:55 +0900

    16:プログラムの追加

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,11 @@ public class Test {
 		// TODO 自動生成されたメソッド・スタブ
 		TestUserDAO dao = new TestUserDAO();
 		dao.select("taro", "123");
+		dao.selectAll();
+		dao.selectByPassword("123");
+		dao.updateUserNameByUserName("taro", "saburo");
+		dao.insert(4, "shiro", "012");
+		dao.delete("jiro");
 	}
 
 }
